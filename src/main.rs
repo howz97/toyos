68379d1c91f0a5dc76017e67558c4b77119816a6
zhanghao <zhanghao997@qq.com>
2024-07-28 17:00:16 +0800

    double fault exception

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,6 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     toy_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-
     #[cfg(test)]
     test_main();
 
