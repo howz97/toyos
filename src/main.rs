d66bf0c7b6be4567253dd6f2216fee0a6d1cd051
zhanghao <zhanghao997@qq.com>
2024-08-04 21:04:18 +0800

    paging

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@
 #![test_runner(toy_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use toy_os::memory;
+use toy_os::memory::BootInfoFrameAllocator;
 use toy_os::println;
+use x86_64::VirtAddr;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     toy_os::init();
     println!("Rust Toy OS!");
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     #[cfg(test)]
     test_main();
